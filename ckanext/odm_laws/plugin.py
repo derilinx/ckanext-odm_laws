8da591ef84d7a0756a544be6e5d59803aed1449e
Alex Corbi <a.corbi@gmail.com>
2016-01-03 16:17:00 +0100

    using same after_update for datasets and resources

--- ckanext/odm_laws/plugin.py
+++ ckanext/odm_laws/plugin.py
@@ -26,7 +26,6 @@ class OdmLawsPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
   plugins.implements(plugins.IPackageController, inherit=True)
   plugins.implements(plugins.IResourceController, inherit=True)
 
-
   def __init__(self, *args, **kwargs):
 
     log.debug('OdmLawsPlugin init')
@@ -68,38 +67,38 @@ class OdmLawsPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     }
 
   def before_create(self, context, resource):
-    log.info('before_create')
-    log.debug("resize:")
+
+    if context['package'].type == 'laws_record':
+      log.info('before_create')
 
   def after_create(self, context, pkg_dict):
 
-    log.debug('after_create: %s', pkg_dict['name'])
+    if context['package'].type == 'laws_record':
+      log.debug('after_create: %s', pkg_dict['name'])
 
-    # Create default Issue
-    review_system = h.asbool(config.get("ckanext.issues.review_system", False))
-    if review_system:
-      if 'type' in pkg_dict:
-        if pkg_dict['type'] == 'laws_record':
-          odm_laws_helper.create_default_issue_laws_record(pkg_dict)
+      # Create default Issue
+      review_system = h.asbool(config.get("ckanext.issues.review_system", False))
+      if review_system:
+        if 'type' in pkg_dict:
+          if pkg_dict['type'] == 'laws_record':
+            odm_laws_helper.create_default_issue_laws_record(pkg_dict)
 
+  def after_update(self, context, pkg_dict_or_resource):
 
+    if context['package'].type == 'laws_record':
+      log.debug('after_update: %s', pkg_dict_or_resource)
 
-  def after_update(self, context, pkg_dict):
-    log.debug('after_update: %s', pkg_dict['name'])
-    # imagemagick here
-    log.debug('salla')
-    #  Create relationship if target is set
-    if 'odm_laws_relationship_target' in pkg_dict:
-      # current dataset
-      rel_subj=pkg_dict['name']
-      # is child of/parent of
-      rel_type=pkg_dict['odm_laws_relationship_type']
-      # relationship target
-      rel_target=pkg_dict['odm_laws_relationship_target']
+      if 'url_type' in pkg_dict_or_resource:
+        # Do resource related logic here
 
-      log.debug("Creating relationship %s %s",rel_type,rel_target)
-      toolkit.get_action('package_relationship_create')(data_dict={'subject': rel_subj,'object':rel_target,'type':rel_type})
+      #  Create relationship if target is set
+      if 'odm_laws_relationship_target' in pkg_dict_or_resource:
+        # current dataset
+        rel_subj=pkg_dict_or_resource['name']
+        # is child of/parent of
+        rel_type=pkg_dict_or_resource['odm_laws_relationship_type']
+        # relationship target
+        rel_target=pkg_dict_or_resource['odm_laws_relationship_target']
 
-  def after_update(self, context, resource):
-    log.debug('after_update (resource): %s', resource['name'])
-    log.debug('resource updated')
+        log.debug("Creating relationship %s %s",rel_type,rel_target)
+        toolkit.get_action('package_relationship_create')(data_dict={'subject': rel_subj,'object':rel_target,'type':rel_type})
